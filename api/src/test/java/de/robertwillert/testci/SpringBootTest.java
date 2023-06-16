6a8522c8ccf496c32251215caab1be63efe134da
Robert Willert <rwillert@users.noreply.github.com>
2023-06-16 16:47:06 +0200

    add postges and test

---
+++ api/src/test/java/de/robertwillert/testci/SpringBootTest.java
@@ -0,0 +1,13 @@
+package de.robertwillert.testci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbooteinstiegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
