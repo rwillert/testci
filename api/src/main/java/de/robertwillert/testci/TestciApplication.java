59ec9156129d010735b6fb89f69777a0ecfd8e8e
Robert Willert <rwillert@users.noreply.github.com>
2023-06-16 17:15:34 +0200

    tix test

---
+++ api/src/main/java/de/robertwillert/testci/TestciApplication.java
@@ -0,0 +1,13 @@
+package de.robertwillert.testci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestciApplication.class, args);
+	}
+
+}
